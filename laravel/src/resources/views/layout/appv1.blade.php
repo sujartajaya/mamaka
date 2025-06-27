54381e0ecab7bb82b4693e1d297cb11c578f9e21
Sujarta Jaya <sujarta.jaya@outlook.co.id>
2025-06-27 12:17:34 +0800

    Mamaka update

--- laravel/src/resources/views/layout/appv1.blade.php
+++ laravel/src/resources/views/layout/appv1.blade.php
@@ -47,6 +47,18 @@
 				</svg>
 			</li>
 			<li><a class="text-sm text-blue-600  font-bold" href="{{ route('tools') }}">Tools</a></li>
+			@if(Auth::check())
+				@if(Auth::User()->type == 'admin')
+				<li class="text-gray-300">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill"
+					viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+				</svg>
+				</li>
+				<li><a class="text-sm text-blue-600  font-bold" href="{{ route('tools') }}">User Admin</a></li>
+				@endif
+			@endif
 		</ul>
 		@if(Auth::check())
 		<form id="logout-form" action="{{ route('logout') }}" method="POST">@csrf</form>
---
+++ laravel/src/resources/views/user/userv1.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.appv1')
+@section('content')
+
+@endsection
